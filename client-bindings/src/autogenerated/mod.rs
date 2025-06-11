0838dd88566bdfd0527648e9dfdd58b8ea313508
tushar-saxena-yral <tushar@gobazzinga.io>
2025-06-11 21:05:08 +0530

    restructure to match rupansh/sats-store

---
+++ client-bindings/src/autogenerated/mod.rs
@@ -0,0 +1,3 @@
+#![allow(clippy::all)]
+#![allow(warnings)]
+pub mod dedup_index;
--- dedup-index/src/lib.rs
+++ dedup-index/src/lib.rs
@@ -1,9 +1,45 @@
-#[allow(warnings)]
-#[cfg(feature = "client")]
-pub mod client;
+use spacetimedb::{ReducerContext, Table, Timestamp, TryInsertError};
 
-#[cfg(feature = "server")]
-pub mod server;
+#[spacetimedb::table(name = dedup_index, public)]
+pub struct VideoHash {
+    #[unique]
+    video_id: String,
+    #[index(btree)]
+    hash: String,
+    created_at: Timestamp,
+}
 
-#[cfg(feature = "server")]
-pub use server::*;
+#[spacetimedb::table(name = unique_hash, public)]
+pub struct UniqueHash {
+    #[primary_key]
+    hash: String,
+    video_id: String,
+}
+
+#[spacetimedb::reducer]
+pub fn add(
+    ctx: &ReducerContext,
+    hash: &str,
+    video_id: &str,
+    created_at: Timestamp,
+) -> utils::Result<()> {
+    utils::validate_sender_identity(ctx, utils::consts::OFFCHAIN_AGENT_TRUSTED_PRINCIPAL)?;
+
+    ctx.db.dedup_index().insert(VideoHash {
+        hash: hash.to_string(),
+        video_id: video_id.to_string(),
+        created_at,
+    });
+
+    let res = ctx.db.unique_hash().try_insert(UniqueHash {
+        hash: hash.to_string(),
+        video_id: video_id.to_string(),
+    });
+
+    match res {
+        Ok(..) | Err(TryInsertError::UniqueConstraintViolation(..)) => Ok(()),
+        _ => {
+            unreachable!("This should never happen")
+        }
+    }
+}
